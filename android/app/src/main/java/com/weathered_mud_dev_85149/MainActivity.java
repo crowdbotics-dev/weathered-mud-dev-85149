5bfdc33fdfbf222d9ae4086ad1f712013a89e317
Crowdbotics <engineering@crowdbotics.com>
2023-05-21 23:08:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weathered_mud_dev_85149/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weathered_mud_dev_85149;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weathered_mud_dev_85149";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-mud-dev-85149",
+  url: "https://weathered-mud-dev-85149.botics.co",
+  api: "https://weathered-mud-dev-85149.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
